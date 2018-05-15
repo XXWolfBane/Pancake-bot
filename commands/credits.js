be72ae10f7a29fe324a07ae2511d4a1c46e38e7b
FHGDev <gideon.lingle14@gmail.com>
2018-05-15 11:10:00 -0500

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -1,10 +1,15 @@
-module.exports.run = (bot, message, args, discord) => {}
+module.exports.run = (bot, message, args, discord) => {
 let em = new discord.RichEmbed()
-.setTitle("People who helped create Pancake Bot!")
-.addField("Owners", "Wolfy!")
-.addField("Developers!", "FHG Dev")
-.setColor("BLUE")
-.setThumbnail(bot.user.avatarURL)
-.setFooter(message.author.username)
-.setTimestamp()
+  .setTitle("People who helped create Pancake Bot!")
+  .addField("Owners", "Wolfy!")
+  .addField("Developers!", "FHG Dev")
+  .setColor("BLUE")
+  .setThumbnail(bot.user.avatarURL)
+  .setFooter(message.author.username)
+  .setTimestamp()
+  message.channel.send({embed: em})
+}
 
+module.exports.help = {
+  name: "credits"
+}
