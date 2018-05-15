e17bed3ccbe57a534e609182c3b517325694e299
XXWolfBane <33186976+XXWolfBane@users.noreply.github.com>
2018-05-15 11:20:03 -0400

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -1,2 +1,6 @@
 module.exports.run = (bot, message, args, discord) => {
-let em = new 
+let em = new discord.RichEmbed()
+.setTitle("People who helped create Pancake Bot!")
+.addField("Owners", "Wolfy!")
+.addField("Developers!", "FHG Dev")
+.setColor("BLUE")
